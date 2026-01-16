31f305fa8231b565fc1a91009e661485a0a257e9
jeswin <jeswinpeter92@gmail.com>
2026-01-16 16:26:47 +0530

    Basic Map View

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -43,6 +43,7 @@ dependencies {
     implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
+    implementation("org.maplibre.gl:android-sdk:11.8.0")
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
